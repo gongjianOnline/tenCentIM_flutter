fdd72d813eb01130c6e3ac162460d690a26ef31a
李承斌 <15031617367@163.com>
2023-07-29 20:40:00 +0800

    完成单聊页面框架搭建

--- lib/app/modules/chart/views/chart_view.dart
+++ lib/app/modules/chart/views/chart_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_im/app/common/myTheme.dart';
 
 import 'package:get/get.dart';
 
@@ -9,15 +10,110 @@ class ChartView extends GetView<ChartController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ChartView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ChartView is working',
-          style: TextStyle(fontSize: 20),
+      body: Container(
+        margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: Column(
+          children: [
+            headerComponent(),
+            Expanded(
+              flex: 1,
+              child: chartComponent()
+            ),
+            inputComponent()
+          ],
         ),
+      )
+    );
+  }
+
+  /* 标题模块 */
+  headerComponent(){
+    return Container(
+      height: 60,
+      padding: const EdgeInsets.only(left: 10),
+      child: Row(
+        children: [
+          InkWell(
+            borderRadius: BorderRadius.circular(20),
+            onTap: (){Get.back();},
+            child: Container(
+              child: const Icon(
+                Icons.arrow_back_ios,
+                color: MyTheme.unimportantFontColor,
+              ),
+            ),
+          ),
+          Container(
+            child: const Text(
+              "南开大学网络教育学院",
+              style: TextStyle(
+                color: MyTheme.stressFontColor,
+                fontSize: 18
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  /**聊天模块 */
+  chartComponent(){
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 243, 243, 243)
+      ),
+      child: ListView(
+        children: [],
+      ),
+    );
+  }
+  /* 输入框 */
+  inputComponent(){
+    return Container(
+      padding: const EdgeInsets.only(left: 10,right: 10,top: 10,bottom: 10),
+      child: Row(
+        children: [
+          /* 语音切换 */
+          InkWell(
+            borderRadius: BorderRadius.circular(20),
+            onTap: (){},
+            child: Container(
+              child:const Icon(
+                Icons.keyboard_voice,
+                color: MyTheme.stressFontColor,
+              ),
+            ),
+          ),
+          /**输入框 */
+          Expanded(
+            flex: 1,
+            child: Container(
+              margin: const EdgeInsets.only(left: 10,right: 10),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(31, 145, 145, 145),
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical:0,horizontal:20), /*调整上下内边距,可改变输入框的高度*/
+                  border: InputBorder.none,
+                ) /*不显示下划线*/ 
+              ),
+            ),
+          ),
+          /* 表情按钮 */
+          InkWell(
+            borderRadius: BorderRadius.circular(20),
+            onTap: (){print("表情");},
+            child: Container(
+              child: const Icon(
+                Icons.emoji_emotions,
+                color: MyTheme.stressFontColor,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,9 +11,9 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
-          color:MyTheme.bgColor
-        ),
+        // decoration: const BoxDecoration(
+        //   color:MyTheme.bgColor
+        // ),
         width: MediaQuery. of (context).size.width,
         child: Column(
           children: [
@@ -69,78 +69,87 @@ class HomeView extends GetView<HomeController> {
 
   /* 单消息模块 */
   messageItem(){
-    return Container(
-      padding: const EdgeInsets.only(left: 10,right: 10),
-      margin: const EdgeInsets.only(bottom: 20),
-      child: Row(
-        children: [
-          /* 头像 */
-          Container(
-            margin: const EdgeInsets.only(right: 10),
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(60),
-              // color: const Color.fromRGBO(228, 228, 228, 1),
-            ),
-            child: Image.asset(
-              "lib/assets/img/user.png",
-              fit: BoxFit.cover,
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              height: 60,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  /*用户名和消息时间 */
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          child: const Text(
-                            "放牛娃",
-                            style: TextStyle(
-                              color: MyTheme.stressFontColor,
-                              fontSize: 18
+    return Ink(
+      child: InkWell(
+        onTap: (){
+          Get.toNamed("/chart");
+        },
+        child: Container(
+          padding: const EdgeInsets.only(left: 10,right: 10),
+          height: 80,
+          // margin: const EdgeInsets.only(bottom: 20),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              /* 头像 */
+              Container(
+                margin: const EdgeInsets.only(right: 10),
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(60),
+                  // color: const Color.fromRGBO(228, 228, 228, 1),
+                ),
+                child: Image.asset(
+                  "lib/assets/img/user.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  height: 60,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      /*用户名和消息时间 */
+                      Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              child: const Text(
+                                "放牛娃",
+                                style: TextStyle(
+                                  color: MyTheme.stressFontColor,
+                                  fontSize: 18
+                                ),
+                              ),
                             ),
+                            Container(
+                              child: const Text(
+                                "15:21",
+                                style: TextStyle(
+                                  color: MyTheme.unimportantFontColor,
+                                  fontSize: 14
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+
+                      /*消息摘要 */
+                      Container(
+                        padding: EdgeInsets.only(right: 50),
+                        child: const Text(
+                          "真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼",
+                          softWrap: false,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: MyTheme.unimportantFontColor,
+                            fontSize: 14
                           ),
                         ),
-                        Container(
-                          child: const Text(
-                            "15:21",
-                            style: TextStyle(
-                              color: MyTheme.unimportantFontColor,
-                              fontSize: 14
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+
+                    ],
                   ),
-
-                  /*消息摘要 */
-                  Container(
-                    padding: EdgeInsets.only(right: 50),
-                    child: const Text(
-                      "真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼真傻逼",
-                      softWrap: false,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        color: MyTheme.unimportantFontColor,
-                        fontSize: 14
-                      ),
-                    ),
-                  )
-
-                ],
-              ),
-            ) 
-          )
-        ],
+                ) 
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
