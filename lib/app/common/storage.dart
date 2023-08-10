c08bd701a57f9dacfc0681c91056217b42196855
李承斌 <15031617367@163.com>
2023-08-10 13:15:47 +0800

    导入了storage封装

---
+++ lib/app/common/storage.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Storage{
+  static setData(String key,dynamic value) async{
+      var prefs = await SharedPreferences.getInstance();
+      prefs.setString(key, json.encode(value));
+  }
+  static getData(String key) async{
+      var prefs=await SharedPreferences.getInstance();
+      String? tempData=prefs.getString(key);
+      if(tempData == null){
+        return null;
+      }else{
+        return json.decode(tempData!);
+      }
+  }
+  static removeData(String key) async{
+      var prefs=await SharedPreferences.getInstance();
+      prefs.remove(key);    
+  }
+}
